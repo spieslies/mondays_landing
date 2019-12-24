d0947e2e540b216e026c388751607b1d5fd7677e
albert_kai <albertkai@me.com>
2019-12-24 22:29:03 +0300

    add plugin

--- artifacts/react-static-templates.js
+++ artifacts/react-static-templates.js
@@ -1,27 +1,10 @@
 
 
-import React from 'react'
-import universal, { setHasBabelPlugin } from '/Users/user/projects/sati_one_landing/node_modules/react-universal-component/dist/index.js'
-
-setHasBabelPlugin()
-
-const universalOptions = {
-  loading: () => null,
-  error: props => {
-    console.error(props.error);
-    return <div>An error occurred loading this page's template. More information is available in the console.</div>;
-  },
-  ignoreBabelRename: true
-}
-
-const t_0 = universal(import('/Users/user/projects/sati_one_landing/node_modules/react-static/lib/browser/components/Default404'), universalOptions)
-      t_0.template = '/Users/user/projects/sati_one_landing/node_modules/react-static/lib/browser/components/Default404'
-      
-
+  
 // Template Map
 export default {
-  '/Users/user/projects/sati_one_landing/node_modules/react-static/lib/browser/components/Default404': t_0
+  '/Users/user/projects/sati_one_landing/node_modules/react-static/lib/browser/components/Default404': require('/Users/user/projects/sati_one_landing/node_modules/react-static/lib/browser/components/Default404').default
 }
-// Not Found Template
-export const notFoundTemplate = "/Users/user/projects/sati_one_landing/node_modules/react-static/lib/browser/components/Default404"
+
+export const notFoundTemplate = '/Users/user/projects/sati_one_landing/node_modules/react-static/lib/browser/components/Default404'
 
--- static.config.js
+++ static.config.js
@@ -1,7 +1,6 @@
 export default {
   plugins: [
     require.resolve('react-static-plugin-reach-router'),
-    require.resolve('react-static-plugin-source-filesystem'),
     require.resolve('react-static-plugin-styled-components'),
   ],
 }
