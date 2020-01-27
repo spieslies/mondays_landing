5ad0d1f18be386dc45601675f4b10e1e007de933
a.nenashev <a.nenashev@qiwi.com>
2020-01-27 13:23:07 +0300

    deacorations

--- src/ui/components/steps/Steps.js
+++ src/ui/components/steps/Steps.js
@@ -7,10 +7,13 @@ import STAR_ICON from "assets/icons/star.svg";
 import BRUSH_C_ICON from "assets/icons/brush_colored.svg";
 import FILE_ICON from "assets/icons/file.svg";
 import SALE_ICON from "assets/icons/sale.svg";
+import STAR_WHITE_ICON from "assets/icons/star_white.svg";
+import SLEEP_IMG from "assets/images/sleep.jpeg";
 
 const SSteps = styled.div`
+  position: relative;
   padding-top: 66px;
-  padding-bottom: 47px;
+  padding-bottom: 132px;
   background-color: ${p => p.theme.colors.bg};
   color: ${p => p.theme.colors.white};
 `;
@@ -35,6 +38,21 @@ const StepItem = styled.div`
   position: relative;
   display: block;
   width: 339px;
+  border-radius: 4px;
+  &:after {
+    content: '';
+    position: absolute;
+    top: 24px;
+    display: block;
+    width: 0;
+    height: 0;
+    border-style: solid;
+    ${p => p.theme.max("md")`
+      display: none;
+    `}
+  };
+  ${p =>
+    p.bg ? `background-color: ${p.bg};` : `background-color: transparent;`}
   &:not(:last-child) {
     margin-bottom: 30px;
     ${p => p.theme.max("md")`
@@ -52,10 +70,23 @@ const StepItem = styled.div`
   ${p =>
     p.ltr
       ? `
+    padding: 21px 30px 31px 20px;
     transform: translateX(-70%);
+    text-align: right;
+    &:after {
+      right: -15px;
+      border-width: 13px 0 13px 16px;
+      border-color: transparent transparent transparent ${p.bg};
+    }
   `
       : `
     transform: translateX(70%);
+    padding: 21px 20px 31px 30px;
+    &:after {
+      left: -15px;
+      border-width: 13px 16px 13px 0;
+      border-color: transparent ${p.bg} transparent transparent;
+    }
   `}
 `;
 const StepItemIconWrapper = styled.div`
@@ -66,17 +97,19 @@ const StepItemIconWrapper = styled.div`
   justify-content: center;
   width: 61px;
   height: 61px;
-  background: transparent;
+  background: red;
   border: 1px solid rgba(187, 107, 217, 0.5);
   border-radius: 100%;
+  ${p =>p.bg ? `background-color: ${p.bg};` : `background-color: transparent;`}
   ${p => p.theme.max("md")`
     position: relative;
-    top: -30px;
+    top: -50px;
     left: auto;
     right: auto;
     margin-left: auto;
     margin-right: auto;
     transform: translateX(0);
+    border: none;
   `};
   &:after {
     content: "";
@@ -89,6 +122,9 @@ const StepItemIconWrapper = styled.div`
     border: 1px solid #373542;
     border-radius: 100%;
     transform: translateY(-50%);
+    ${p => p.theme.max("md")`
+      ${p =>p.bg ? `border-color: ${p.bg};` : `border-color: #373542;`}
+    `}
   }
   ${p =>
     p.ltr
@@ -124,6 +160,95 @@ const StepItemDescription = styled.div`
   `}
 `;
 
+const LastStepItem = styled.div`
+  position: relative;
+  margin-top: 25px;
+  padding: 51px 85px 51px 235px;
+  border-radius: 7px;
+  max-width: 720px;
+  display: block;
+  background-color: ${p => p.theme.colors.primary};
+  ${p => p.theme.max("md")`
+    width: 100%;
+    padding: 30px;
+    text-align: center;
+    margin-top: 350px;
+  `}
+  ${p => p.theme.between("sm", "md")`
+    width: 60%;
+  `}
+`;
+
+const LastStepItemText = styled.span`
+  font-size: 18px;
+  line-height: 38px;
+`;
+
+const LastStepItemStars = styled.div`
+  display: flex;
+  margin-bottom: 25px;
+  ${p => p.theme.max("md")`
+    justify-content: center;
+  `}
+`;
+
+const LastStepItemStar = styled.img`
+  width: 17p;
+  height: 17px;
+  display: block;
+  &:not(:last-child) {
+    margin-right: 15px;
+  }
+`;
+
+const LastStepItemStarImg = styled.img`
+  position: absolute;
+  left: -200px;
+  top: 50%;
+  display: block;
+  width: 375px;
+  height: 430px;
+  border-radius: 7px;
+  object-fit: cover;
+  transform: translateY(-50%);
+  z-index: 101;
+  &:not(:last-child) {
+    margin-right: 15px;
+  }
+  ${p => p.theme.max("md")`
+    margin: 0;
+    transform: translateX(-50%);
+    left: 50%;
+    width: 110%;
+    top: -390px;
+    height: 400px;
+  `}
+`;
+
+const StepsBottom = styled.div`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  display: block;
+  height: 110px;
+  width: 30%;
+  background: #1f2033;
+  border-top-right-radius: 50px;
+  z-index: 100;
+  &:after {
+    content: "";
+    position: absolute;
+    bottom: 0;
+    right: -60px;
+    display: block;
+    width: 60px;
+    height: 60px;
+    background: transparent;
+    border-radius: 60px;
+    box-shadow: -30px 30px 0 5px #1f2033;
+  }
+`;
+
 class Steps extends PureComponent {
   render() {
     const { t } = this.props;
@@ -139,8 +264,8 @@ class Steps extends PureComponent {
               <br />
               создания марафонов
             </SubTitle>
-            <StepItem ltr>
-              <StepItemIconWrapper ltr>
+            <StepItem ltr bg="#BB6BD9">
+              <StepItemIconWrapper ltr bg="#BB6BD9">
                 <StepItemIcon src={STAR_ICON} />
               </StepItemIconWrapper>
               <StepItemTitle>
@@ -152,8 +277,8 @@ class Steps extends PureComponent {
                 марафон. Формат также предполагает каждодневные задания
               </StepItemDescription>
             </StepItem>
-            <StepItem>
-              <StepItemIconWrapper>
+            <StepItem bg="#8B4AA4">
+              <StepItemIconWrapper bg="#8B4AA4">
                 <StepItemIcon src={BRUSH_C_ICON} />
               </StepItemIconWrapper>
               <StepItemTitle>
@@ -166,8 +291,8 @@ class Steps extends PureComponent {
                 дизайна
               </StepItemDescription>
             </StepItem>
-            <StepItem ltr>
-              <StepItemIconWrapper ltr>
+            <StepItem ltr bg="#6B387E">
+              <StepItemIconWrapper ltr bg="#6B387E">
                 <StepItemIcon src={FILE_ICON} />
               </StepItemIconWrapper>
               <StepItemTitle>Добавьте контент на каждый день</StepItemTitle>
@@ -177,8 +302,8 @@ class Steps extends PureComponent {
                 марафон. Формат также предполагает каждодневные задания
               </StepItemDescription>
             </StepItem>
-            <StepItem>
-              <StepItemIconWrapper>
+            <StepItem bg="#532864">
+              <StepItemIconWrapper bg="#532864">
                 <StepItemIcon src={SALE_ICON} />
               </StepItemIconWrapper>
               <StepItemTitle>Установите цену и другие параметры</StepItemTitle>
@@ -188,8 +313,23 @@ class Steps extends PureComponent {
                 количество возможных пропусков и т.п.
               </StepItemDescription>
             </StepItem>
+            <LastStepItem>
+              <LastStepItemStarImg src={SLEEP_IMG} />
+              <LastStepItemStars>
+                <LastStepItemStar src={STAR_WHITE_ICON} />
+                <LastStepItemStar src={STAR_WHITE_ICON} />
+                <LastStepItemStar src={STAR_WHITE_ICON} />
+                <LastStepItemStar src={STAR_WHITE_ICON} />
+                <LastStepItemStar src={STAR_WHITE_ICON} />
+              </LastStepItemStars>
+              <LastStepItemText>
+                Опубликуйте ваш марафон, и он станет доступен для тысяч людей. И
+                будет приносить Вам деньги даже когда вы спите
+              </LastStepItemText>
+            </LastStepItem>
           </Flex>
         </Container>
+        <StepsBottom/>
       </SSteps>
     );
   }
